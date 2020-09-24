425d48315da5fa71db70b30785b089ff51d60987
shivisingh28 <shivi2819997@gmail.com>
2020-09-25 01:29:41 +0530

    first commit

---
+++ background.js
@@ -0,0 +1,25 @@
+defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+];
+
+
+
+
+chrome.webRequest.onBeforeRequest.addListener(
+	function (details) {
+		return {
+			cancel: true,
+		};
+	},
+	{ urls: defaultFilters },
+	["blocking"] //regex
+);
